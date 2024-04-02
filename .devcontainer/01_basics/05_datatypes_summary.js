3b6f20443699bfb270cc68f6f84643575689fa5d
Ankur Katara <105636922+katara007@users.noreply.github.com>
2024-04-02 06:36:18 +0000

    datatypes summary

---
+++ .devcontainer/01_basics/05_datatypes_summary.js
@@ -0,0 +1,33 @@
+// Primitive
+
+// 7 Types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outSideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId= Symbol('123')
+
+// console.log(id === anotherId);
+
+
+// Javascript is Dynamically Typed language
+
+// Reference (non-Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = { 
+    name:"ankur",
+    age:32,
+}
+const myFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof myObj);
