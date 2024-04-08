bfa6b6abe79b63cdcd043e38d0a81427f374faac
Ankur Katara <105636922+katara007@users.noreply.github.com>
2024-04-08 05:21:22 +0000

    functions

--- .devcontainer/03_Basics/02_function.js
+++ .devcontainer/03_Basics/02_function.js
@@ -27,7 +27,13 @@ function loginUserMessage(username){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("ankur"))
+// console.log(loginUserMessage("ankur"))
+
+function calculateCartPrice(num1){
+    return num1
+}
+
+console.log(ca);
 
 function loginUserMessage(username){
     if (username === undefined) {
@@ -47,4 +53,45 @@ function loginUserMessage(username = "sam"){
     }
     return `${username} just logged in`
 }
-console.log(loginUserMessage("ankur"));
+// console.log(loginUserMessage("ankur"));
+
+function calculateCartPrice(num1) {
+    return num1
+}
+
+// console.log(calculateCartPrice(2));
+
+function calculateCartPrice(...num1) {   //(...)=> Rest Operator=> convert the value in Array 
+    return num1
+}
+
+// console.log(calculateCartPrice(220, 33, 400, 2100));
+
+
+function calculateCartPrice(val1, val2, ...num1) {   // val1, val2 => took only two values from the input
+    return num1
+}
+// console.log(calculateCartPrice(220, 33, 400, 2100));
+
+const user = {
+    username: "Ankur",
+    price: 100
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username}, and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sandy",
+    price: 299
+})
+
+const myNewArray = [233, 455, 6, 443]
+
+function getSecondArray(getArray) {
+    return getArray[3]
+}
+
+console.log(getSecondArray(myNewArray));
