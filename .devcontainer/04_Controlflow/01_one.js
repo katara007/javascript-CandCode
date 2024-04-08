073b4d0b81c529c27427117e92791f6ee8efe7f2
Ankur Katara <105636922+katara007@users.noreply.github.com>
2024-04-08 10:19:00 +0000

    If

---
+++ .devcontainer/04_Controlflow/01_one.js
@@ -0,0 +1,24 @@
+// If
+const isUserloggedIn = true
+const temperature = 41
+
+// if ( 2 == "2") {
+//     console.log("Executed");
+// }
+if ( 2 === "2") {
+    // console.log("Executed");
+}
+
+// >, <, >=, <=, ==, !=, ===
+
+// if ( temperature < 50) {
+//     console.log("Less than 50");
+// }
+// console.log("greater than 50");
+
+if ( temperature < 50) {
+    console.log("Less than 50");
+} else {
+    console.log("greater than 50");
+}
+
