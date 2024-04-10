1d53f0c07bb23853768adf3bfe908eec6780d58a
Ankur Katara <105636922+katara007@users.noreply.github.com>
2024-04-10 13:16:44 +0000

    if and else

--- .devcontainer/04_Controlflow/01_one.js
+++ .devcontainer/04_Controlflow/01_one.js
@@ -16,9 +16,43 @@ if ( 2 === "2") {
 // }
 // console.log("greater than 50");
 
-if ( temperature < 50) {
-    console.log("Less than 50");
-} else {
-    console.log("greater than 50");
+// if ( temperature < 50) {
+//     console.log("Less than 50");
+// } else {
+//     console.log("greater than 50");
+// }
+// console.log("Execute");
+
+
+const score = 200
+
+// if (score > 100) {
+//     const power = "fly" // cannot use var becoz it is global scope
+//     console.log(`user power: ${power}`);
+// }
+
+const balance = 1000
+// if (balance > 500) console.log("test");
+
+// if (balance < 500) {
+//     console.log("less then");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard) {
+    console.log("allow to buy course");
 }
 
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("user log in");
+}
