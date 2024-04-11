cf8758a68bc2e09988d7e981c7a320918286d7cc
Ankur Katara <105636922+katara007@users.noreply.github.com>
2024-04-11 04:37:05 +0000

    while loop

---
+++ .devcontainer/05_iterations/02_while.js
@@ -0,0 +1,26 @@
+
+let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ["flash", "superman", "batman"]
+
+let arr = 1
+// while (arr < myArray.length) {
+//     console.log(`My value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+// let score = 1
+// do {
+//     console.log(`my score is ${score}`);
+//     score++
+// } while (score <= 10);
+
+let score = 11
+do {
+    console.log(`my score is ${score}`); // work first then look for condition
+    score++
+} while (score <= 10);
